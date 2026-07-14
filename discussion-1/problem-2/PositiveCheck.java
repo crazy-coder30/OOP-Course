effe4f20c4b354ec7b5aab5891083f16c049fc22
Aritro <aritroghosh200828@gmail.com>
2026-07-14 13:47:23 +0600

    Add discussion-1/problem-2: Check if the number is positive or not

---
+++ discussion-1/problem-2/PositiveCheck.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PositiveCheck{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if(num>=0){
+            System.out.println("The number is positive");
+        }
+        else{
+            System.out.println("The number is not a positive a number");
+        }
+
+        sc.close();
+    }
+}
