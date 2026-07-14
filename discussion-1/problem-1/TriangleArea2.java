c6bf9a86801c7cad75ef7004b4f23579484d28a6
Aritro <aritroghosh200828@gmail.com>
2026-07-14 12:33:09 +0600

    Add discussion-1/problem-1: Area of a Triangle

---
+++ discussion-1/problem-1/TriangleArea2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TriangleArea2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Ener 1st arm: ");
+        double arm1 = sc.nextDouble();
+
+        System.out.print("Ener 2nd arm: ");
+        double arm2 = sc.nextDouble();
+
+        System.out.print("Ener 3rd arm: ");
+        double arm3 = sc.nextDouble();
+
+        double s = (arm1 + arm2 + arm3) / 2.0;
+
+        double area = Math.sqrt( s * (s - arm1) * (s - arm2) * (s - arm3) );
+
+        System.out.println("Area of the triangle: " + area);
+
+        sc.close();
+    }
+}
